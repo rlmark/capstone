8f4331c48dbafb8b3ee3418462f419f36f408d25
Rebecca Mark <rebecca.lmark@gmail.com>
2015-02-06 15:29:50 -0800

    front page mic display block works

---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,11 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+
+// show demo on click
+$( document ).ready(function() {
+  $("#big-mic-home").click(function(event){
+    $("#bio-form").fadeIn();
+    $(".big-mic").css({display: 'block'})
+  });
+});
