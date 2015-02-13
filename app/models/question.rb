c60c015a54893954e41842e116f6ccde291b122b
Rebecca Mark <rebecca.lmark@gmail.com>
2015-02-12 21:28:40 -0800

    added question mark check

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,4 +4,10 @@ class Question < ActiveRecord::Base
   accepts_nested_attributes_for :talking_points
 
   validates :content, presence: true, length: {minimum: 3}
+
+  def question_mark_check
+    if self.content[-1] != "?"
+      self.content += "?"
+    end
+  end
 end
--- app/models/talking_point.rb
+++ app/models/talking_point.rb
@@ -1,5 +1,7 @@
 class TalkingPoint < ActiveRecord::Base
   belongs_to :question
 
-  validates :phrase, presence: true
+  validates :phrase, presence: true, length: {minimum: 3}, allow_blank: false, :allow_nil => false
+  # validates_presence_of :phrase
+
 end
